29e79942a1cc3919c16118d45e19fa9d98e38055
luca gastaldi <lucagastaldi82@gmail.com>
2023-12-15 14:56:07 +0000

    completed with help from the internet

--- 3/day_three_2.rb
+++ 3/day_three_2.rb
@@ -1,3 +1,5 @@
+require "set"
+
 input =
 [
 "467..114..",
@@ -13,12 +15,14 @@ input =
 
 # input = File.readlines("day_three_input.txt", chomp: true)
 
-
+rows = input.map do |row|
+  row
+end
 
 def find_stars(input)
   stars = []
   input.each_with_index do |string, string_index|
-    string.chars.each_with_index do |char, star_index|
+    string.each_char.with_index do |char, star_index|
       if char == "*"
         stars << [string_index, star_index]
       end
@@ -27,56 +31,45 @@ def find_stars(input)
   stars
 end
 
-def check_same_string(input, star)
-
-  star_string = star.first
-  star_index = star.last
-  string = input[star_string]
-
-  matches = []
-
-  if string[star_index - 1].match?(/\d/)
-    before = string.chars.shift(star_index).reverse.join.match(/\d+/)
-    matches << before[0]
-  end
-
-  if string[star_index + 1].match?(/\d/)
-    after = string.chars.pop(star_index).join.match(/\d+/)
-    matches << after[0]
-  end
-  matches
-end
-
-def check_previous_string(input, star)
-  star_string = star.first
-  star_index = star.last
-  previous_string = input[star_string - 1]
-
-  if previous_string
-    matches = []
-    match_area = [star_index - 1, star_index, star_index + 1]
-    numbers_indices = previous_string.enum_for(:scan, /\d+/).map { Regexp.last_match.begin(0) }
-    numbers = previous_string.scan(/\d+/)
-    numbers_location = []
-    numbers.each_with_index do |number, index|
-      length = number.length
-      start = numbers_indices[index]
-      finish = numbers_indices[index + length - 1]
-      numbers_location << ((start..finish).to_a)
-    end
-
-    numbers_location.each do |number|
-      p number
-    end
-
-  end
-
-end
-
-
 stars = find_stars(input)
 
-stars.each do |star|
-  check_same_string(input, star)
-  check_previous_string(input, star)
+
+gear_ratios = []
+stars.each do |(x, y)|
+  number_locations = []
+	[
+		[-1, -1],
+		[-1, 0],
+		[-1, 1],
+		[0, -1],
+		[0, 1],
+		[1, -1],
+		[1, 0],
+		[1, 1]
+	].each do |(dx, dy)|
+		nx = x + dx
+		ny = y + dy
+		if rows[nx][ny] =~ /\d/
+			number_locations << [nx, ny]
+		end
+	end
+
+  number_starts = Set.new
+
+  number_locations.each do |(x, y)|
+    while rows[x][y - 1] =~ /\d/
+      y -= 1
+    end
+    number_starts << [x, y]
+  end
+
+  if number_starts.count == 2
+    result = number_starts.map do |(x, y)|
+      rows[x][y..].to_i
+    end.inject(:*)
+    gear_ratios << result
+
+  end
 end
+
+p gear_ratios.sum
